cdfa0a17cf146e6df8350c13491ce4a06c068b6c
Mohandgalou06 <galoumoumouh07@gmail.com>
2026-04-04 10:20:16 +0200

    fix DatabaseManager args

--- app.py
+++ app.py
@@ -30,10 +30,9 @@ CORS(app)
 import os
 db = DatabaseManager(
     host=os.environ.get("MYSQLHOST", "localhost"),
-    port=int(os.environ.get("MYSQLPORT", 3306)),
     user=os.environ.get("MYSQLUSER", "root"),
     password=os.environ.get("MYSQLPASSWORD", ""),
-    database=os.environ.get("MYSQLDATABASE", "puissance4")
+    database=os.environ.get("MYSQLDATABASE", "railway")
 )
 DB_OK = db.connect()
 
