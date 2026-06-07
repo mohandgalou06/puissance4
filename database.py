c98effa2126dd4b42c379f5a9062b132fe889d0e
Mohandgalou06 <galoumoumouh07@gmail.com>
2026-06-08 00:54:06 +0200

    Nouveau projet Puissance4

--- database.py
+++ database.py
@@ -20,8 +20,8 @@ class Database:
         self.config = {
             "host": os.getenv("DB_HOST", "localhost"),
             "user": os.getenv("DB_USER", "root"),
-            "password": os.getenv("DB_PASSWORD", ""),
-            "database": os.getenv("DB_NAME", "puissance4"),
+            "password": os.getenv("DB_PASSWORD", "Galou1646!"),
+            "database": os.getenv("DB_NAME", "puissance4_btvs"),
             "port": int(os.getenv("DB_PORT", "3306")),
         }
         self.conn = None
